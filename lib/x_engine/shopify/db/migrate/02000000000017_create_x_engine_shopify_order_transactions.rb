d2f01a7d9a6d0c49432239b28609eed6855bc377
Richard Peck <rpeck@fl.co.uk>
2026-09-01 21:48:05 +0100

    Updated Migrations

--- lib/x_engine/shopify/db/migrate/02000000000017_create_x_engine_shopify_order_transactions.rb
+++ lib/x_engine/shopify/db/migrate/02000000000017_create_x_engine_shopify_order_transactions.rb
@@ -40,7 +40,11 @@ class CreateXEngineShopifyOrderTransactions < XEngine::Core::Database::Migration
   #
   # @return [void]
   def up
-    create_table table_name, **table_options do |t|
+    # Allocate bigint to id column to override the global UUID default strategy.
+    # Ensures we are able to use the numeric GID from Shopify as the naked table primary key identifier.
+    localized_options = table_options.merge(id: :bigint, default: nil)
+
+    create_table table_name, **localized_options do |t|
 
       # Strict relationship binding to the parent order record
       t.belongs_to :order,
